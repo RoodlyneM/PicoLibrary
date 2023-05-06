d0233d30fcfa18882fdc547a11d5bf49629c990f
RoodlyneM <132164431+RoodlyneM@users.noreply.github.com>
2023-05-06 17:24:17 -0400

    added buzzer on increment and red light on reset

--- Counter.py
+++ Counter.py
@@ -1,42 +1,48 @@
 from Displays import *
 from Button import *
+from Buzzer import *
+from Lights import Light
+import time
+""" This is a counter class that willl implement basic
+increment reset and show count on a display """
 
-"""
-This is a counter that will implement a basic increment reset 
-counter and show the count display.
-"""
-
-
-
-class counter:
+class Counter:
     def __init__(self):
-        print("Counter: Constructor")
+        print("Counter: Contstructor")
         self._number = 0
-        self._display = display = LCDDisplay(sda=20, scl=21, i2cid=0)
+        self._display = LCDDisplay(sda = 20, scl = 21, i2cid = 0)
+        self._greenButton = Button(17, "increase", buttonhandler = self, lowActive=True)
+        self._redButton = Button(16, "reset", buttonhandler = self, lowActive=True)
+        self._buzzer = PassiveBuzzer(13)
+        self._light = Light(0, "Red light")
 
-        self._greenButton = Button(17, "increase", buttonhandler = self, lowActive = True)
-        self._reButton = Button(16, "reset", buttonhandler = self, lowActive = True)
-
-    def increment (self):
-        print("Counter: Incrementing")
+    def increment(self):
+        print("Counter: Increment")
         self._number = self._number + 1
+        self._buzzer.beep()
 
+
+   
     def reset(self):
-        print("I should be reseting")
+        print("Counter: reset")
         self._number = 0
         self._display.reset()
+        self._light.on()
+        time.sleep(0.1)
+        self._light.off()
 
-    def buttonPressed(self, name):
+    def buttonPressed(self,name):
         if name == "increase":
             self.increment()
+            
         elif name == "reset":
             self.reset()
 
-    def buttonReleased (self, name):
+
+    def buttonReleased(self,name):
         pass
-    
+
     def show(self):
         while True:
             self._display.showNumber(self._number)
             time.sleep(0.1)
-            
